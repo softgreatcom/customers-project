5275a27003ad8ab93e5cf2548ec4b5f2e58f008a
VitalySavich <vitaly@softgreat.com>
2018-02-19 09:21:26 +0300

    Component based implementation

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -7,4 +7,60 @@ angular.module('dashboardApp')
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       });
+  }).component("customersList",{
+      controller: function ($scope, $rootScope, $http) {
+
+        $scope.customers = [];
+
+        $http.get('/api/customers').then(function(customers) {
+          $scope.customers = customers.data;
+        });
+
+        function selectCustomer(customer) {
+
+            // First approach inter-component communication
+            // using broadcast event
+            $rootScope.$broadcast('SelectCustomer', customer);
+            debugger;
+            onEventRaised();
+
+            // Second approach inter-component communication
+            // using rootScope function
+            $rootScope.selectCustomerFunction(customer);
+        }
+
+        $scope.selectCustomer = selectCustomer;
+
+      },
+      templateUrl: "../../components/customer-list/customer-list.html",
+  }).component("customersListContainer",{
+      controller: function ($scope, $rootScope, $http) {
+
+        $scope.selectedCustomer = {
+        		name: "Tiger Dejesus",
+        		email: "congue.elit@enimdiamvel.com",
+        		company: "Vel Arcu PC",
+        		region: "Nord-Pas-de-Calais",
+        		zipCode: "11615",
+        		info: "sem. Nulla interdum. Curabitur dictum. Phasellus in felis. Nulla tempor",
+        		country: "Tokelau",
+        		phone: "(03874) 8192714",
+        		city: "Hénin-Beaumont",
+        		organisationNumber: "87979648499"
+        };
+
+        // First approach inter-component communication
+        // using broadcast event
+        $rootScope.$on('SelectCustomer', function(event, customer) {
+          $scope.selectedCustomer = customer;
+        });
+
+        // Second approach inter-component communication
+        // using rootScope function
+        $rootScope.selectCustomerFunction = function(customer){
+          $scope.selectedCustomer = customer;
+        };
+
+      },
+      templateUrl: "../../components/customer-list-container/customer-list-container.html",
   });
